ec0dbd991025532d610c87b7a99c7a6b23cca36d
Blaed <blaedj@gmail.com>
2012-11-05 19:31:44 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DuckingDuck::Application.config.secret_token = '1e630bbb53f3d10c5fcabf8991b1913a02dbdd03ad73ffdbf4ddf83ee506b6fa30dc0f070b33f564ce6d8f152e5d320a997768fa063d56a3531a5ed3f81d6c72'
